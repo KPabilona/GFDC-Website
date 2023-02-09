e5660a2a1e4a0c7cd8e0d3840b91d333422e2420
Kevin Pabilona <kevinmarkongcoy@gmail.com>
2023-02-09 11:13:14 +0800

    About landing page done

--- FRONT-END/js/LandingPage.js
+++ FRONT-END/js/LandingPage.js
@@ -5,3 +5,8 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+// const navbar = document.querySelector("nav");
+// window.addEventListener ("scroll", function() {
+//     nav.classList.toggle ("sticky", window.scrollY > 100);
+// });
