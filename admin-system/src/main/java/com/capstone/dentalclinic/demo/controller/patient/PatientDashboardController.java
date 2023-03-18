45c77d9530502194ec856db731f5e0b5cb7796dc
Johndavidcabal <Johndavidcabal000@gmail.com>
2023-03-18 12:10:44 +0800

    Minor changes in mailsender

--- admin-system/src/main/java/com/capstone/dentalclinic/demo/controller/patient/PatientDashboardController.java
+++ admin-system/src/main/java/com/capstone/dentalclinic/demo/controller/patient/PatientDashboardController.java
@@ -1,6 +1,10 @@
 package com.capstone.dentalclinic.demo.controller.patient;
 
 import lombok.AllArgsConstructor;
+
+import java.security.Principal;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PatientDashboardController {
 
     @GetMapping("/dashboard")
-    public String patientDashboard() {
+    public String patientDashboard(Authentication authentication) {
+        System.out.println("This is Authentication of the user");
+        System.out.println(authentication.getDetails());
+        
         return "/PatientWebPages/PatientDashboard";
     }
 
